b3fe36283481add76b892249200676980a4b775b
RaximovDoston <rdoston22@gmail.com>
2021-01-30 14:17:24 +0500

    changes

--- resources/views/front/layout.blade.php
+++ resources/views/front/layout.blade.php
@@ -5,8 +5,8 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <!-- <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"> -->
   <!-- <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css"> -->
-  <link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css" />
+  <link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet">
   <link rel="stylesheet" href="{{ asset('style-carousel.css') }}">
   <link rel="stylesheet" href="{{ asset('style.css') }}">
   <title>Sciensoft Development Company</title>
