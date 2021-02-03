9bf2038cdd6f7d33668d318ae8cbc5bd08a848f5
Uktam1998 <rakhmatullayev2019@mail.ru>
2021-02-03 10:26:42 +0500

    cardlist addet to home page

--- app/Http/Controllers/HomePageController.php
+++ app/Http/Controllers/HomePageController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Category;
+use App\Models\CardList;
 use Illuminate\Support\Facades\View;
 
 class HomePageController extends Controller
@@ -13,7 +14,8 @@ class HomePageController extends Controller
         $categories = Category::whereNull('category_id')
             ->with('childrenCategories')
             ->get();
-        return view('front.pages.index', ['categories'=> $categories, 'page'=>'front.pages.index']);
+        $cardlists = CardList::orderBy('id', 'desc')->get();
+        return view('front.pages.index', ['categories'=> $categories, 'page'=>'front.pages.index', 'cardlists'=>$cardlists]);
     }
     public function getPage($page, $translation='en')
     {
@@ -24,7 +26,26 @@ class HomePageController extends Controller
         // return view('front.pages.'.$page, compact('categories'));
         return View::make('front.layout', ['page' => $page]);
     }
+    public function postConsultation(Request $request){
 
+        $this->validate($request, [
+            'fullname' => 'required',
+            'company' => 'required',
+            'email' => 'required|email',
+            'phone_number' => 'required|numeric',
+            'description' => 'required',
+        ]);
+
+        $posts = new Consultation();
+        $posts->fullname = $request->fullname;
+        $posts->company = $request->company;
+        $posts->email = $request->email;
+        $posts->phone_number = $request->phone_number;
+        $posts->description = $request->description;
+        $posts->save();
+        
+        return redirect('/')->with('success', 'Your information has been sent successfully!');
+    }
 
     public function getBlade($page, $translation='en')
     {
@@ -35,6 +56,7 @@ class HomePageController extends Controller
         if(!view()->exists('front.pages.'.$page)){
             return view('front.pages.index', ['categories'=> $categories, 'page'=>'front.pages.index']);
         }
+        $cardlists = CardList::orderBy('id', 'desc')->get();
         return view('page_controller', ['page'=>'front.pages.'.$page, 'categories'=>$categories]);
     }
 
