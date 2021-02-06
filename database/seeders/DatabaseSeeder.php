71802c3ecd817eef5a0dc680cd47fd7bb2e9ec8a
unknown <erkinovbegzod.45@gmail.com>
2021-02-06 21:02:51 +0500

    changes

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -108,5 +108,60 @@ class DatabaseSeeder extends Seeder
         }
         // Category::insert($categories);
         // DB::table('categories')->insert($categories);
+
+        DB::table('languages')->delete();
+
+        DB::table('languages')->insert(array (
+            0 =>
+            array (
+                'id' => 1,
+                'name' => 'English',
+                'code' => 'en',
+                'rtl' => 0,
+                'created_at' => '2019-01-20 17:13:20',
+                'updated_at' => '2019-01-20 17:13:20',
+            ),
+            1 =>
+            array (
+                'id' => 2,
+                'name' => "O'zbekcha",
+                'code' => 'uz',
+                'rtl' => 0,
+                'created_at' => '2019-02-17 11:35:37',
+                'updated_at' => '2019-02-18 11:49:51',
+            ),
+            2 =>
+            array (
+                'id' => 3,
+                'name' => 'Русский',
+                'code' => 'ru',
+                'rtl' => 1,
+                'created_at' => '2019-04-28 23:34:12',
+                'updated_at' => '2019-04-28 23:34:12',
+            ),
+        ));
+
+        DB::table('translations')->delete();
+
+        DB::table('translations')->insert(array (
+            0 =>
+            array (
+                'id' => 3,
+                'lang' => 'en',
+                'lang_key' => 'All Category',
+                'lang_value' => 'All Category',
+                'created_at' => '2020-11-02 12:40:38',
+                'updated_at' => '2020-11-02 12:40:38',
+            ),
+            1 =>
+            array (
+                'id' => 4,
+                'lang' => 'en',
+                'lang_key' => 'All',
+                'lang_value' => 'All',
+                'created_at' => '2020-11-02 12:40:38',
+                'updated_at' => '2020-11-02 12:40:38',
+            ),
+        ));
     }
 }
--- resources/views/layouts/menu.blade.php
+++ resources/views/layouts/menu.blade.php
@@ -32,3 +32,7 @@
     <a class="nav-link" href="{{ route('products.index') }}"><i class="fas fa-building"></i><span>Products</span></a>
 </li>
 
+<li class="side-menus {{ Request::is('languages*') ? 'active' : '' }}">
+    <a class="nav-link" href="{{ route('languages.index') }}"><i class="fas fa-building"></i><span>Languages</span></a>
+</li>
+
