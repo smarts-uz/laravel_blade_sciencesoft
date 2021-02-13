ae870a666b27467952c7011072b9b35d8bec4492
Turgunov188 <t.jominic@gmail.com>
2021-02-13 12:25:48 +0500

    bug fixed index blade

--- resources/views/front/pages/index.blade.php
+++ resources/views/front/pages/index.blade.php
@@ -712,8 +712,6 @@
           </div>
         </div>
       </div>
-
->>>>>>>>> Temporary merge branch 2
     </div>
 
 
@@ -1017,26 +1015,6 @@
   <div class="px-16 py-12 bg-gray-200 text-gray-800">
     <label class="">Drop us a line! We are here to answer your questions 24/7.</label>
     <div class="flex flex-col lg:flex-row justify-between">
-      <form  method="POST" action="{{ route('consultation') }}">
-        @csrf
-        <div class="flex flex-col mr-3">
-          <div class="flex justify-between flex-row flex-wrap">
-            <input type="text" name="fullname" placeholder="Full Name"
-              class="border-2 border-gray-500 outline-none my-3 mr-3 p-4 w-full sm:w-auto md:w-2/5 xl:w-auto" />
-            <input type="text" name="company" placeholder="Company"
-              class="border-2 border-gray-500 outline-none  py-4 px-3 my-3 mr-3 w-full sm:w-auto md:w-2/5 xl:w-auto" />
-            <input type="email" name="email" placeholder="Work Email"
-              class="border-2 border-gray-500 outline-none  py-4 px-3 my-3 mr-3 w-full sm:w-auto md:w-2/5 xl:w-auto">
-            <input type="text" name="phone_number" placeholder="Work Phone"
-              class="border-2 border-gray-500 outline-none my-3 mr-3 py-4 px-3 w-full sm:w-auto md:w-2/5 xl:w-auto">
-          </div>
-          <textarea placeholder="How can we help you?" name="description" cols="3" rows="6"
-            class="border-2 border-gray-500 outline-none my-3 -mr-3 py-4 px-3" style="margin-right: 0.75rem;"></textarea>
-           <div class="flex justify-center items-center mb-3">
-              <button type="submit" class="text-center uppercase bg-blue-700 hover:bg-blue-900 text-white w-52 h-12">Discuss my needs</button>
-           </div>
-        </div>
-      </form>
   <form method="POST" action="{{ route('consultation') }}" class="px-16 py-12 bg-gray-200 text-gray-800">
     @csrf
     <label class="">Drop us a line! We are here to answer your questions 24/7.</label>
