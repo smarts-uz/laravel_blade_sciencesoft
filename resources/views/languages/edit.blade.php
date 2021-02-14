f6f2e5d187137511cf64d9dfb43f86a00335e15a
iBekzod <erkinovbegzod.45@gmail.com>
2021-02-15 01:28:36 +0500

    translation changed demo use

--- resources/views/languages/edit.blade.php
+++ resources/views/languages/edit.blade.php
@@ -3,28 +3,28 @@
 @section('content')
 
 <div class="aiz-titlebar text-left mt-2 mb-3">
-    <h5 class="mb-0 h6">{{translate('Language Information')}}</h5>
+    <h5 class="mb-0 h6">{{translate_lang('Language Information')}}</h5>
 </div>
 
 <div class="col-lg-6 mx-auto">
     <div class="card">
         <div class="card-header">
-            <h5 class="mb-0 h6">{{translate('update Language Info')}}</h5>
+            <h5 class="mb-0 h6">{{translate_lang('update Language Info')}}</h5>
         </div>
         <div class="card-body p-0">
             <form class="p-4" action="{{ route('languages.update', $language->id) }}" method="POST">
                 @csrf
                 <div class="form-group row">
                     <div class="col-lg-3">
-                        <label class="control-label">{{ translate('Name') }}</label>
+                        <label class="control-label">{{ translate_lang('Name') }}</label>
                     </div>
                     <div class="col-lg-9">
-                        <input type="text" class="form-control" name="name" placeholder="{{ translate('Name') }}" value="{{ $language->name }}" required>
+                        <input type="text" class="form-control" name="name" placeholder="{{ translate_lang('Name') }}" value="{{ $language->name }}" required>
                     </div>
                 </div>
                 <div class="form-group row">
                     <div class="col-lg-3">
-                        <label class="control-label">{{ translate('Code') }}</label>
+                        <label class="control-label">{{ translate_lang('Code') }}</label>
                     </div>
                     <div class="col-lg-9">
                         <select class="form-control aiz-selectpicker mb-2 mb-md-0" name="code" data-live-search="true" >
@@ -39,7 +39,7 @@
                 </div>
 
                 <div class="form-group mb-0 text-right">
-                    <button type="submit" class="btn btn-sm btn-primary">{{translate('Save')}}</button>
+                    <button type="submit" class="btn btn-sm btn-primary">{{translate_lang('Save')}}</button>
                 </div>
             </form>
         </div>
