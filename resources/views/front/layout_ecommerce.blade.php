a70fe04f5ea022bc38637e87399aa7432a6e586a
OgabekYuldoshev <yuldashoff1@gmail.com>
2021-02-17 12:18:35 +0500

    ecommerce

---
+++ resources/views/front/layout_ecommerce.blade.php
@@ -0,0 +1,65 @@
+
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <!-- <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"> -->
+  <!-- <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css"> -->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css" />
+  <link rel="preconnect" href="https://fonts.gstatic.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com">
+<link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;1,100;1,200;1,300&display=swap" rel="stylesheet">
+  <link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet">
+  <link rel="stylesheet" href="{{ asset('style-carousel.css') }}">
+  <link rel="stylesheet" href="{{ asset('style.css') }}">
+  <link rel="stylesheet" href="{{ asset('cardHoverStyle.css') }}">
+  <title>Sciensoft Development Company</title>
+
+  </head>
+  <body>
+
+    <div class="wrapper">
+    @include('front.Components.ecommerce_navbar')
+
+    <div class="mt-32">
+        @yield('main')
+    </div>
+
+    @include('front.Components.footer')
+    </div>
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.9.0/slick.min.js"></script>
+<script src="{{ asset('index.js') }}"></script>
+<script>
+  $(document).ready(function () {
+      $('.customer-logos').slick({
+        slidesToShow: 6,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 1000,
+        arrows: false,
+        dots: false,
+        pauseOnHover: false,
+        responsive: [{
+          breakpoint: 768,
+          setting: {
+            slidesToShow: 4
+          }
+        }, {
+          breakpoint: 520,
+          setting: {
+            slidesToShow: 3
+          }
+        }]
+      });
+    });
+</script>
+
+</body>
+</html>
+
+
