2c90edeafb62bd53a5373c4b4ef8f80cda61cde2
Muhriddin <muxriddin.murodov@mail.ru>
2021-02-20 14:20:43 +0500

    ecommmerce

---
+++ resources/views/front/pages/Services/Solutions/Ecommerces/BLOG/e-commerce_blog_two.blade.php
@@ -0,0 +1,309 @@
+
+@extends('front.layout')
+
+@section('css')
+
+@endsection
+
+@section('main')
+
+
+<div class="container mx-auto">
+
+    <div class="text-center mt-10">
+        <strong class="text-4xl">
+            Bring Real-World Experience Back with 3D Shopping and a Virtual Reality Store
+        </strong>
+
+        <div class="rounded-full h-24 mt-6 flex justify-center">
+            <img class="" src="images/t-yablonskaya-1_-_1439w.png" alt="">
+        </div>
+
+        <strong class="text-blue-900">Tanya Yablonskaya</strong>
+        <p class="text-lg">
+            Ecommerce Industry Analyst, ScienceSoft</p>
+
+        <hr class="mt-3.5">
+
+
+    </div>
+
+    <div class="mt-6">
+        <p class="text-xl">
+            <i>
+                <b> Editor’s </b>note: We explore the distinctive features of a virtual store that boost customer
+                satisfaction.
+                If you consider developing an ecommerce solution that would provide genuinely immersive customer
+                experience, you are welcome to leverage ScienceSoft’s custom ecommerce development services.
+
+            </i>
+        <p class="mt-3.5 text-xl">
+            To convert and successfully retain customers, a retailer has to provide them with a compelling shopping
+            experience. Conventional online shopping with lengthy browsing of numerous product lists can be
+            perceived as a chore, while virtual shopping can be a way more personal, enjoyable experience. A virtual
+            store is still an emerging form of ecommerce, but it’s gaining in popularity at quite a rapid pace – an
+            estimated 100 million people are already using VR and AR to shop.
+
+        </p>
+        </p>
+    </div>
+
+    <div class="mt-6">
+        <strong class="text-3xl">
+            Virtual store – a quick intro
+        </strong>
+
+        <p class="mt-3.5 text-xl">
+            Virtual stores employ computer simulation technologies to create the virtual environment of a physical
+            store. A customer can walk around the store, interact with accurate 3D models of products, instantly add
+            them to a virtual shopping cart, and more.
+
+        </p>
+
+    </div>
+
+    <div class="flex mt-16">
+        <div class="w-28">
+            <img src="images/quotation-mark.svg" alt="">
+        </div>
+
+        <div>
+            <p class="text-2xl mt-10 ml-3.5">
+                3D virtual stores, AR applications, and virtual reality stores are three major types of virtual shops.
+                Their difference lies in the technologies implemented and the resulting level of a customer’s
+                engagement.
+            </p>
+        </div>
+    </div>
+
+    <div class="mt-10 flex justify-center">
+        <img src="images/3d-shopping.png" alt="">
+    </div>
+    <div>
+        <div class="flex flex-wrap mt-16">
+            <div>
+                <div class="h-2.5 w-2.5 bg-gray-900 mt-3.5 rounded-full"></div>
+            </div>
+            <div>
+                <b class="text-3xl ml-3.5">3D virtual store</b>
+            </div>
+        </div>
+        <p class="text-xl mt-3.5">
+            You can power an ecommerce application with 3D shopping experience so that customers move in a virtual shop,
+            browse and zoom any product they like. No additional devices are required. To get a clearer impression of
+            what experience the 3D technology provides, read about one of ScienceSoft’s projects – 3D virtual store
+            development for an ecommerce retailer with various product categories.
+
+
+        </p>
+    </div>
+
+    <div>
+        <div class="flex flex-wrap mt-16">
+            <div>
+                <div class="h-2.5 w-2.5 bg-gray-900 mt-3.5 rounded-full"></div>
+            </div>
+            <div>
+                <b class="text-3xl ml-3.5">AR application</b>
+            </div>
+        </div>
+        <p class="text-xl mt-3.5">
+            Ecommerce brands can use AR applications to provide for informed product choices and enhance the after-sale
+            experience for their customers. In this article, my colleague Anastasia Yaskevich shares details on how
+            augmented reality can be incorporated into a shopping routine and bring an element of fun into the shopping
+            process.
+
+        </p>
+    </div>
+
+    <div>
+        <div class="flex flex-wrap mt-16">
+            <div>
+                <div class="h-2.5 w-2.5 bg-gray-900 mt-3.5 rounded-full"></div>
+            </div>
+            <div>
+                <b class="text-3xl ml-3.5">Virtual reality store</b>
+            </div>
+        </div>
+        <p class="text-xl mt-3.5">
+            Virtual reality shopping has manifested in retail as a way to create a more immersive experience for
+            customers. Though its application in ecommerce is questionable due to special equipment such as a VR headset
+            and hand controllers required, the technology can differentiate customers’ in-store experience. For example,
+            retailers may offer a virtual tour across a large mall or welcome customers to a behind-the-scenes tour to
+            see how products are produced.
+        </p>
+    </div>
+
+    <div class="container mt-16 shadow-xl border border-black-600 mx-auto">
+        <div class="p-10     flex  flex-wrap">
+            <div>
+                <div>
+                    <strong class="text-3xl">
+                        Hesitate over the Cost of a Virtual Ecommerce Solution?
+                    </strong>
+                    <div class="w-14 h-0.5 bg-yellow-600 mt-3.5"></div>
+                </div>
+
+                <p class=" mt-4 text-xl">ScienceSoft’s team will be pleased to elaborate on an implementation plan and
+                    estimate <br> its cost for you.
+                </p>
+            </div>
+
+
+            <div class="ml-24">
+                <button class="bg-blue-900 text-xl hover:bg-blue-700 rounded w-64 mt-5 h-14  text-white">
+
+                    Request cost estimation
+                </button>
+
+            </div>
+        </div>
+    </div>
+
+    <div class="mt-6">
+        <strong class="text-3xl">
+            How a virtual store brings offline purchasing experience online
+        </strong>
+
+        <p class="mt-3.5 text-xl">
+            Combining the capabilities of both offline and online shopping, a virtual online store brings an informative
+            and joyful element of real shopping to online retail. Here are some of the opportunities that virtual
+            shopping opens to customers.
+        </p>
+
+    </div>
+
+
+    <div>
+        <div class="flex flex-wrap mt-16">
+            <div>
+                <div class="h-2.5 w-2.5 bg-gray-900 mt-3.5 rounded-full"></div>
+            </div>
+            <div>
+                <b class="text-3xl ml-3.5">Getting a true-to-life representation of a product</b>
+            </div>
+        </div>
+        <p class="text-xl mt-3.5">
+            Customers are more likely to make a purchase after they receive all information they need, and a virtual
+            store provides an opportunity to try a product before buying. A buyer can pick the interactive 3D model of a
+            product, examine it from different angles, and even preview in real surroundings, which results in a careful
+            and informed product choice and fewer product returns.
+        </p>
+    </div>
+
+    <div>
+        <div class="flex flex-wrap mt-16">
+            <div>
+                <div class="h-2.5 w-2.5 bg-gray-900 mt-3.5 rounded-full"></div>
+            </div>
+            <div>
+                <b class="text-3xl ml-3.5">Getting instant customer help</b>
+            </div>
+        </div>
+        <p class="text-xl mt-3.5">
+            Just like in a physical store, customers can get help during online shopping and after the purchase is made.
+            For example, customers can turn to a virtual shopping assistant and have their questions answered straight
+            away.
+        </p>
+    </div>
+
+    <div>
+        <div class="flex flex-wrap mt-16">
+            <div>
+                <div class="h-2.5 w-2.5 bg-gray-900 mt-3.5 rounded-full"></div>
+            </div>
+            <div>
+                <b class="text-3xl ml-3.5">Making online shopping a social activity</b>
+            </div>
+        </div>
+        <p class="text-xl mt-3.5">
+            Several people can simultaneously share a virtual environment and interact with it in real time, which makes
+            for a multiuser experience. Customers can visit a visually appealing virtual online store with their
+            friends, which makes shopping a social activity available at home.
+        </p>
+    </div>
+
+
+    <div>
+        <div class="flex flex-wrap mt-16">
+            <div>
+                <div class="h-2.5 w-2.5 bg-gray-900 mt-3.5 rounded-full"></div>
+            </div>
+            <div>
+                <b class="text-3xl ml-3.5">Delight your buyers with virtual shopping</b>
+            </div>
+        </div>
+        <p class="text-xl mt-3.5">
+            Most of the advantages featured by virtual stores are not actually new, as they often constitute customer
+            experience in brick-and-mortar retail. Their originality comes from incorporating those elements into an
+            ecommerce solution and blurring the boundaries between the two retail channels. As a result, customers can
+            fully experience products and perceive shopping as an engaging activity that they will be glad to repeat.
+        <p class="text-xl mt-3.5">
+            ScienceSoft has hands-on experience of integrating virtual reality technologies into the ecommerce realm. If
+            you are thinking of launching a virtual online store and need help with bringing the idea to life, you are
+            welcome to contact our ecommerce team for consulting and development assistance.
+        </p>
+
+        </p>
+    </div>
+
+    <div class="hover:shadow-xl mt-14 mx-16 mt-6 border border-black-600 mb-16">
+        <div class="flex flex-wrap">
+            <div class="w-72 mt-5 mb-5 ml-5 h-44">
+                <img src="images/ecommerce_development_services.png" alt="">
+
+            </div>
+
+            <div class="w-6/12">
+                <div class="ml-20 mb-6 mt-6">
+                    <strong class="text-2xl">
+                        Magento Migration and Development for a Seller of Printed and E-Books
+                    </strong>
+                    <div class="w-14 h-0.5 bg-yellow-600 mt-3"></div>
+
+
+                    <p class="text-xl mt-3.5">ScienceSoft’s team migrated two Magento 1 websites with about 90% of
+                        custom code
+                        to Magento 2 and merged them into a single multistore.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="flex justify-center lg:mx-0 lg:flex-row flex-col flex-wrap ">
+        <div class="w-3/12 mr-10 hover:shadow-xl border-b-4 hover:border-blue-600 transition delay-150">
+            <img src="images/furniture-ecommerce_-_1439w.png" alt="">
+            <p class="text-xl mt-3.5 p-4 ">
+                <b>
+                    How to Launch a Web Store and Sell Furniture Online with Success
+                </b></p>
+        </div>
+        <div class="w-3/12 mr-10 hover:shadow-xl border-b-4 hover:border-blue-600 transition delay-150">
+            <img src="images/ecommerce-personalization_-_1439w.png" alt="">
+            <p class="text-xl mt-3.5 p-4 ">
+                <b>
+                    How to Launch a Web Store and Sell Furniture Online with Success
+                </b></p>
+        </div>
+        <div class="w-3/12 mr-10 hover:shadow-xl border-b-4 hover:border-blue-600 transition delay-150">
+            <img src="images/4-faces-of-healthcare-ecommerce_-_1439w.png" alt="">
+            <p class="text-xl mt-3.5 p-4 ">
+                <b>
+                    How to Launch a Web Store and Sell Furniture Online with Success
+                </b></p>
+        </div>
+    </div>
+</div>
+
+
+@endsection
+
+@section('js')
+
+@endsection
+
+
+
+
+
+
