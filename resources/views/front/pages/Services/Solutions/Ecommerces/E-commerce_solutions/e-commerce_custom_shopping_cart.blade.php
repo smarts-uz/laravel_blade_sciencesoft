89cf0cac6daecb08f7c0183231c338d23ab27d34
Muhriddin <muxriddin.murodov@mail.ru>
2021-02-20 11:43:24 +0500

    ecommmm

---
+++ resources/views/front/pages/Services/Solutions/Ecommerces/E-commerce_solutions/e-commerce_custom_shopping_cart.blade.php
@@ -0,0 +1,27 @@
+
+@extends('front.layout')
+
+@section('css')
+
+@endsection
+
+@section('main')
+
+<div class="text-5xl">
+
+    <h1>
+        Custom Shopping Cart
+    </h1>
+</div>
+
+@endsection
+
+@section('js')
+
+@endsection
+
+
+
+
+
+
---
+++ resources/views/front/pages/Services/Solutions/Ecommerces/E-commerce_solutions/e-commerce_multi-store_ecommerce.blade.php
@@ -0,0 +1,27 @@
+
+@extends('front.layout')
+
+@section('css')
+
+@endsection
+
+@section('main')
+
+<div class="text-5xl">
+
+    <h1>
+        Multi-Store Ecommerce
+    </h1>
+</div>
+
+@endsection
+
+@section('js')
+
+@endsection
+
+
+
+
+
+
