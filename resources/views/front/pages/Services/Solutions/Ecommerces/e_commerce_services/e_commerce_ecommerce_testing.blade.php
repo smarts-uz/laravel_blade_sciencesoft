6711fcfa46393c5cb1d65351bb55caf547e31d36
Akbarov90 <ilyasovich706@gmail.com>
2021-02-22 18:05:49 +0300

    changed by Akbarov90

--- resources/views/front/pages/Services/Solutions/Ecommerces/e_commerce_services/e_commerce_ecommerce_testing.blade.php
+++ resources/views/front/pages/Services/Solutions/Ecommerces/e_commerce_services/e_commerce_ecommerce_testing.blade.php
@@ -7,12 +7,270 @@
 
 @section('main')
 
-<div class="text-5xl">
+<div class="">
+    <h1 class="uppercase font-bold mt-10 text-3xl">Ecommerce Testing Services</h1>
+    <hr class="w-12 border-b-2 border-yellow-600  my-4"/>
+    <div>
+        <img src="https://www.scnsoft.com/ecommerce/ecommerce-testing.svg" alt="">
+    </div>
+    <p class="text-xl lg:text-2xl text-justify mt-4">Even when browsing through your ecommerce website, your potential
+        customers can be click away from going to your
+        competitors. An unreliable ecommerce solution when your target audience faces bugs, security and functionality
+        breaches, website unavailability will certainly reduce your sales level. Moreover, it’s a serious threat to your
+        reputation.
+    </p>
+    <p class="text-xl lg:text-2xl text-justify mt-4">Spending money on ecommerce solutions, you need to be sure that
+        they work for the benefit of your business.
+    </p>
+    <p class="text-xl lg:text-2xl text-justify mt-4 ">ScienceSoft is ready to test your ecommerce solution and thus help
+        you establish smooth online sales to win and
+        retain customer attention.
+    </p>
+    <button type="button"
+            class="focus:outline-none bg-blue-700 text-white w-full uppercase mt-4 py-3 md:w-72 md:block ml-auto md:mt-6">
+        request ecommerce testing
+    </button>
 
-    <h1>
-        Ecommerce Testing
-    </h1>
+    <h1 class="uppercase font-bold mt-10 text-3xl mt-8">WHAT WE TEST</h1>
+    <hr class="w-12 border-b-2 border-yellow-600  my-4"/>
+
+    <p class="text-xl lg:text-2xl text-justify mt-4 ">We test multiple aspects of your ecommerce solution to make sure
+        that it is well-performing from the inside and
+        attractive from the outside, which will help to keep your customers engaged and enjoying their shopping
+        experience.
+    </p>
+
+
+
+        <div class="w-full hover:shadow-lg border p-6 mt-4">
+        <div class="lg:flex ">
+            <div class="  mx-auto lg:mt-6 lg:mr-5 lg:w-2/12">
+                <img src="https://www.scnsoft.com/blue-icons-on-grey-background/frontend.svg" alt=""
+                     class=" lg:flex mx-auto  ">
+            </div>
+            <div class="w-10/12">
+            <h1 class="uppercase font-bold mt-10 text-xl mt-8">
+                Web and mobile front end of an ecommerce solution
+            </h1>
+            <hr class="w-12 border-b-2 border-yellow-600  my-4"/>
+            <p class="text-xl lg:text-2xl text-justify mt-4 ">We simulate customer journeys and evaluate user experience
+                that an ecommerce solution can provide. We check whether graphical user interface (GUI) elements (icons,
+                widgets, etc.) are displayed properly, goods are sorted and displayed correctly and with right
+                descriptions, customers are redirected to the right pages, and so on.</p>
+               </div>
+       </div>
+        </div>
+
+        <div class="w-full hover:shadow-lg border p-6 my-4">
+          <div class="lg:flex ">
+            <div class="  mx-auto lg:mt-6 lg:mr-5 lg:w-2/12">
+                <img src="https://www.scnsoft.com/blue-icons-on-grey-background/ux.svg" alt=""
+                     class=" mx-auto  " width="90" height="90">
+            </div>
+           <div class="lg:w-10/12">
+            <h1 class="uppercase font-bold mt-10 text-xl mt-8">
+                Usability on all types of devices
+            </h1>
+            <hr class="w-12 border-b-2 border-yellow-600  my-4"/>
+            <p class="text-xl lg:text-2xl text-justify mt-4 ">
+            We study whether your ecommerce solution offers
+             the best possible user experience with the variety of devices your
+              customers may use (smartphones, tablets, computers) in the variety of environments.
+                </div>
+
+               </p>
 </div>
+        </div>
+          <div class="w-full hover:shadow-lg border p-6 my-4">
+                    <div class="lg:flex ">
+                   <div class="  mx-auto lg:mt-6 lg:mr-5 lg:w-2/12">
+                        <img src="https://www.scnsoft.com/blue-icons-on-grey-background/ecommerce.svg" alt=""
+                             class=" mx-auto ">
+                    </div>
+                   <div class="lg:w-10/12">
+                    <h1 class="uppercase font-bold mt-10 text-xl mt-8">
+                       Functionality of an ecommerce solution
+                    </h1>
+                    <hr class="w-12 border-b-2 border-yellow-600  my-4"/>
+                    <p class="text-xl lg:text-2xl text-justify mt-4 ">We test such aspects as shopping cart functionality (adding items
+                    to the cart and removing them,
+                    applying discount codes and so on), order processing and tracking, payment processing and more. </p>
+                    </div>
+</div>
+                </div>
+
+               <div class="w-full hover:shadow-lg border p-6 my-4">
+               <div class="lg:flex ">
+                             <div class="mx-auto lg:w-2/12 lg:mt-6 lg:mr-5">
+                                        <img src="https://www.scnsoft.com/blue-icons-on-grey-background/analytics-chart.svg" alt=""
+                                                 width="90" height="90" class="mx-auto">
+                                    </div>
+                                   <div class="lg:w-10/12">
+                                        <h1 class="uppercase font-bold mt-10 text-xl mt-8">
+                                         Ecommerce analytics
+
+                                  </h1>
+
+                                    <hr class="w-12 border-b-2 border-yellow-600  my-4"/>
+                                        <p class="text-xl lg:text-2xl text-justify mt-4 ">We test whether your databases and data
+                                        warehouses are prepared to receive and store huge flows of data generated by your customers. We also check
+                                        whether the data analytics components of your ecommerce solution are tuned to generate meaningful and not misleading insights
+                                         from the data about your customers, their behavior and their purchases, the proportion of your ecommerce
+                                        website visitors that turn into your customers, and so on.</p>
+                                        </div>
+</div>
+                                </div>
+
+
+
+
+
+   <div class="w-full hover:shadow-lg border p-6 my-4">
+     <div class="lg:flex ">
+
+                           <div class="  mx-auto lg:mt-6 lg:mr-5 lg:w-2/12 ">
+                               <img src="https://www.scnsoft.com/blue-icons-on-grey-background/maximum-speed.svg" alt=""
+                                   class="block mx-auto " width="90" height="90">
+                           </div>
+<div class="lg:w-10/12">
+                           <h1 class="uppercase font-bold mt-10 text-xl mt-8">
+                             Ecommerce website performance
+
+                         </h1>
+                           <hr class="w-12 border-b-2 border-yellow-600  my-4"/>
+                           <p class="text-xl lg:text-2xl text-justify mt-4 ">We test whether your ecommerce website stands high loads, has no
+                           performance issues when goods’ prices and descriptions are updated, and so on. We can also provide quick <a href="#" class="underline hover:text-blue-700">
+                           performance testing of your solution</a>   before sales,
+                           e.g., the Black Friday sale.
+
+ </p>
+ </div>
+ </div>
+
+
+                       </div>
+
+
+
+
+
+       <div class="w-full hover:shadow-lg border p-6 my-4">
+                        <div class="lg:flex">
+                         <div class=" block mx-auto lg:w-2/12 lg:mt-6 lg:mr-5">
+                             <img src="https://www.scnsoft.com/blue-icons-on-grey-background/integration.svg" alt=""
+                                  class=" mx-auto :w-2/12">
+                         </div>
+                         <div class="lg:w-10/12">
+                         <h1 class="uppercase font-bold mt-10 text-xl mt-8">
+                          Integration of an ecommerce solution with other company systems
+                         </h1>
+                         <hr class="w-12 border-b-2 border-yellow-600  my-4"/>
+                         <p class="text-xl lg:text-2xl text-justify mt-4 ">We test how your ecommerce solution is connected to other
+                         systems responsible for multiple sales-related operations (payment gateways, PIM, CRM, delivery management system) and
+                         check whether the data
+                         is transferred without delays to and from your ecommerce solution.</p>
+</div>
+</div>
+
+                     </div>
+
+
+
+
+                    <div class="w-full hover:shadow-lg border p-6 my-4">
+                      <div class="lg:flex">
+                                            <div class=" block mx-auto lg:w-2/12 lg:mt-6 lg:mr-5">
+                                                <img src="https://www.scnsoft.com/blue-icons-on-grey-background/security-check.svg" alt=""
+                                                     class="block mx-auto " width="90" height="90">
+                                            </div>
+                                           <div class="lg:w-10/12">
+                                            <h1 class="uppercase font-bold mt-10 text-xl mt-8">
+                                            Ecommerce security
+
+                                           </h1>
+                                            <hr class="w-12 border-b-2 border-yellow-600  my-4"/>
+                                            <p class="text-xl lg:text-2xl text-justify mt-4 ">We identify and assess security vulnerabilities in your solution, conduct
+                                            penetration testing to imitate a real attack and offer recommendations for reducing security risks. We can test your ecommerce
+                                            solution using
+                                             black-box, white-box and grey-box methods.</p>
+</div>
+
+
+                                        </div>
+
+
+
+                                        </div>
+
+
+
+
+<h1 class="uppercase font-bold mt-10 text-3xl mt-8">How We Can Cooperate</h1>
+    <hr class="w-12 border-b-2 border-yellow-600  my-4"/>
+
+<div class="lg:grid grid-cols-3 gap-8">
+    <div class="my-5 ">
+        <div>
+            <img src="https://www.scnsoft.com/ecommerce/independent-testing-during-implementation.svg" alt=""
+                 width="200"
+                 height="200">
+        </div>
+        <h4 class="text-2xl font-bold mt-6">Independent testing during implementation</h4>
+        <p class="text-xl lg:text-2xl text-justify mt-4 ">Before the launch of an ecommerce website or a customer
+            application you’ve developed, we ensure that all the components of your solution (search functionality
+            engines,
+            billing systems, databases and so on) are integrated and perform their functions well, comply with the
+            security
+            standards regulations and perform well even under heavy loads.</p>
+    </div>
+
+
+    <div class="my-5 text-justify">
+        <div>
+            <img src="https://www.scnsoft.com/ecommerce/acceptance-testing.svg" alt="" width="200"
+                 height="200">
+        </div>
+        <h4 class="text-2xl font-bold mt-6">Acceptance testing</h4>
+        <p class="text-xl lg:text-2xl text-justify mt-4 ">We impartially test your ecommerce solution from the end user
+            perspective and check its compliance with your business requirements and security standards.</p>
+    </div>
+
+    <div class="my-5 text-justify">
+        <div>
+            <img src="https://www.scnsoft.com/ecommerce/managed-testing-services_2.svg" alt="" width="200"
+                 height="200">
+        </div>
+        <h4 class="text-2xl font-bold mt-6">Managed testing services</h4>
+        <p class="text-xl lg:text-2xl text-justify mt-4 ">We provide ecommerce testing at each stage of developing an
+            ecommerce solution: from testing project requirements to upgrade and migration testing. Also, when a
+            solution is implemented, we are ready to regularly assess whether business processes continue to work as
+            intended.</p>
+    </div>
+</div>
+
+
+
+</div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 @endsection
 
