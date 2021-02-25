bdc0538b3b8ce7468b3df44a4a4100d7ec53b65c
Muhriddin <muxriddin.murodov@mail.ru>
2021-02-25 14:15:02 +0500

    page

--- resources/views/front/pages/about/company/management_team.blade.php
+++ resources/views/front/pages/about/company/management_team.blade.php
@@ -23,10 +23,56 @@
                         {{ $team->description }}
                     </p>
                 </div>
+
+
+                 <!-- <div class="my-1 px-1 w-full overflow-hidden sm:w-1/2 md:w-1/2 lg:w-1/3 xl:my-2 xl:px-2 xl:w-1/3">
+                    <img src="/uploads/teams/{{ $team->image ?? asset('images/default-image.png') }}"
+                        class="w-36 mb-3 mx-auto" />
+                    <p class="text-gray-900 font-bold text-base tracking-wide text-xl mb-3 text-center">
+                        {{ $team->name }}
+                    </p>
+                    <p class="text-gray-400 text-lg text-center">
+                        {{ $team->job }}
+                    </p>
+                    <p class="font-normal text-lg text-gray-700 tracking-wide mt-3">
+                        {{ $team->description }}
+                    </p>
+                </div> -->
+
+
+                 <!-- <div class="my-1 px-1 w-full overflow-hidden sm:w-1/2 md:w-1/2 lg:w-1/3 xl:my-2 xl:px-2 xl:w-1/3">
+                    <img src="/uploads/teams/{{ $team->image ?? asset('images/default-image.png') }}"
+                        class="w-36 mb-3 mx-auto" />
+                    <p class="text-gray-900 font-bold text-base tracking-wide text-xl mb-3 text-center">
+                        {{ $team->name }}
+                    </p>
+                    <p class="text-gray-400 text-lg text-center">
+                        {{ $team->job }}
+                    </p>
+                    <p class="font-normal text-lg text-gray-700 tracking-wide mt-3">
+                        {{ $team->description }}
+                    </p>
+                </div> -->
+
+
+                 <!-- <div class="my-1 px-1 w-full overflow-hidden sm:w-1/2 md:w-1/2 lg:w-1/3 xl:my-2 xl:px-2 xl:w-1/3">
+                    <img src="/uploads/teams/{{ $team->image ?? asset('images/default-image.png') }}"
+                        class="w-36 mb-3 mx-auto" />
+                    <p class="text-gray-900 font-bold text-base tracking-wide text-xl mb-3 text-center">
+                        {{ $team->name }}
+                    </p>
+                    <p class="text-gray-400 text-lg text-center">
+                        {{ $team->job }}
+                    </p>
+                    <p class="font-normal text-lg text-gray-700 tracking-wide mt-3">
+                        {{ $team->description }}
+                    </p>
+                </div> -->
             @endforeach
         </div>
     </div>
-@endsection
+
+
 
 
 @section('js')
