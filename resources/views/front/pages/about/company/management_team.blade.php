9e421a7615caad87bf59d907909f386d43b093e8
Shohruh <shohruhqudratullev2455@gmail.com>
2021-02-25 17:27:43 +0500

    add full page

--- resources/views/front/pages/about/company/management_team.blade.php
+++ resources/views/front/pages/about/company/management_team.blade.php
@@ -8,7 +8,7 @@
     <!--Komil Sobitov/ Team Management -->
     <div class="my-10">
         <h1 class="text-blue-700 text-2xl md:text-4xl  font-extrabold">{{ _trans('Management Team') }}</h1>
-        <div class="flex flex-wrap mx-1 overflow-hidden justify-center items-start xl:-mx-2">
+        <div class="flex flex-wrap mx-1 overflow-hidden mt-4 justify-center items-start xl:-mx-2">
             {{-- @foreach ($teams as $team)
                   <div class="my-1 px-1 w-full overflow-hidden sm:w-1/2 md:w-1/2 lg:w-1/3 xl:my-2 xl:px-2 xl:w-1/3">
                     <img src="/uploads/teams/{{ $team->image ?? asset('images/default-image.png') }}"
@@ -24,35 +24,31 @@
                     </p>
                 </div>
             @endforeach --}}
-            <label class="font-bold text-xl pr-2" for="technology">Industries:</label>
-            {{-- <div class="grid grid-cols-1 items-center">
-                <div x-data="{open:false}"
-                    class="p-2 border-none relative outline-none bg-transparent text-blue-500 text-xl font-bold" name="industry" id="industry">
-                    <div class="text-blue-400 w-60 cursor-pointer flex justify-between items-center">All <i class="fas fa-chevron-down"></i></div>
-                    <ul x-show="open" class="absolute border border-gray-200 bg-white z-30">
-                        <a href="">
-                            <li class="text-black hover:bg-blue-400 hover:text-white duration-100 px-5 py-1">
-                            </li>
-                        </a>
-                    </ul>
-                </div>
-            </div> --}}
-            <div class="grid grid-cols-4 gap-5">
+            <div class="grid grid-cols-1 lg:grid-cols-4 gap-5 mt-12">
                 <div>
-                    <a href="linkedin">
-                        <img src="{{asset('assets/images/MurodovMuhriddin.jpg')}}" alt="">
+                    <a href="https://www.linkedin.com/in/muhriddin-mmo-aaa4071a0/">
+                        <img class="rounded	" src="{{asset('assets/images/MurodovMuhriddin.jpg')}}" alt="">
                         <div class="hover:text-red-700 uppercase">
-                            <h1 class="text-xl font-normal">Mike santoro</h1>
-                            <h1 class="text-xl font-normal">CEO</h1>
+                            <h1 class="text-xl font-bold">Mike santoro</h1>
+                            <h1 class="text-base font-normal">CEO</h1>
                         </div>
                     </a>
                 </div>
                 <div>
                     <a href="linkedin">
-                        <img src="{{asset('assets/images/MurodovMuhriddin.jpg')}}" alt="">
+                        <img class="rounded	" src="{{asset('assets/images/MurodovMuhriddin.jpg')}}" alt="">
                             <div class="hover:text-red-700 uppercase">
-                            <h1 class="text-xl font-normal">Mike santoro</h1>
-                            <h1 class="text-xl font-normal">CEO</h1>
+                            <h1 class="text-xl font-bold">Mike santoro</h1>
+                            <h1 class="text-base font-normal">CEO</h1>
+                        </div>
+                    </a>
+                </div>
+                <div>
+                    <a href="linkedin">
+                        <img class="rounded	" src="{{asset('assets/images/MurodovMuhriddin.jpg')}}" alt="">
+                        <div class="hover:text-red-700 uppercase">
+                            <h1 class="text-xl font-bold">Mike santoro</h1>
+                            <h1 class="text-base font-normal">CEO</h1>
                         </div>
                     </a>
                 </div>
@@ -60,8 +56,37 @@
                     <a href="linkedin">
                         <img src="{{asset('assets/images/MurodovMuhriddin.jpg')}}" alt="">
                         <div class="hover:text-red-700 uppercase">
-                            <h1 class="text-xl font-normal">Mike santoro</h1>
-                            <h1 class="text-xl font-normal">CEO</h1>
+                            <h1 class="text-xl font-bold">Mike santoro</h1>
+                            <h1 class="text-base font-normal">CEO</h1>
+                        </div>
+                    </a>
+                </div>
+            </div>
+            <div class="grid grid-cols-1 lg:grid-cols-4 gap-5 mt-12">
+                <div>
+                    <a href="linkedin">
+                        <img class="rounded	" src="{{asset('assets/images/MurodovMuhriddin.jpg')}}" alt="">
+                        <div class="hover:text-red-700 uppercase">
+                            <h1 class="text-xl font-bold">Mike santoro</h1>
+                            <h1 class="text-base font-normal">CEO</h1>
+                        </div>
+                    </a>
+                </div>
+                <div>
+                    <a href="linkedin">
+                        <img class="rounded	" src="{{asset('assets/images/MurodovMuhriddin.jpg')}}" alt="">
+                            <div class="hover:text-red-700 uppercase">
+                            <h1 class="text-xl font-bold">Mike santoro</h1>
+                            <h1 class="text-base font-normal">CEO</h1>
+                        </div>
+                    </a>
+                </div>
+                <div>
+                    <a href="linkedin">
+                        <img class="rounded	" src="{{asset('assets/images/MurodovMuhriddin.jpg')}}" alt="">
+                        <div class="hover:text-red-700 uppercase">
+                            <h1 class="text-xl font-bold">Mike santoro</h1>
+                            <h1 class="text-base font-normal">CEO</h1>
                         </div>
                     </a>
                 </div>
@@ -69,8 +94,8 @@
                     <a href="linkedin">
                         <img src="{{asset('assets/images/MurodovMuhriddin.jpg')}}" alt="">
                         <div class="hover:text-red-700 uppercase">
-                            <h1 class="text-xl font-normal">Mike santoro</h1>
-                            <h1 class="text-xl font-normal">CEO</h1>
+                            <h1 class="text-xl font-bold">Mike santoro</h1>
+                            <h1 class="text-base font-normal">CEO</h1>
                         </div>
                     </a>
                 </div>
