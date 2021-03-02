5b42145ec5bafdbdb78fe1dcf2a5e044bf9b3d16
Turgunov188 <t.jominic@gmail.com>
2021-03-02 16:12:32 +0500

    appservice

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -1,9 +1,9 @@
 <?php
 
 namespace App\Providers;
-
+use Illuminate\Support\Facades\Blade;
 use Illuminate\Support\ServiceProvider;
-
+//use App\View\Components\project;
 class AppServiceProvider extends ServiceProvider
 {
     /**
@@ -26,6 +26,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        //Blade::component('project', project::class);
     }
 }
