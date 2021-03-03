cf01d451e150ce238d02e0f53ab76d2b4b845486
Turgunov188 <t.jominic@gmail.com>
2021-03-03 10:19:50 +0500

    managent team tag add

--- resources/views/front/pages/About/Company/management_team.blade.php
+++ resources/views/front/pages/About/Company/management_team.blade.php
@@ -7,34 +7,13 @@
 @section('main')
     <div class="py-20 text-center  -mb-24 pb-24 overflow-hidden">
         <h1 class="text-blue-700 text-2xl md:text-4xl py-8  font-extrabold">{{ _trans('Our Team') }}</h1>
-        <div class="flex flex-wrap md:justify-evenly lg:justify-evenly">
-            <div class="flex items-center">
-                <label class="font-bold text-xl pr-2" for="technology">Filter by Department:</label>
-                <div x-data="{open:false}"  class="p-2 border-none relative outline-none bg-transparent text-blue-500 text-xl font-bold" name="technology" id="technology">
-                    <div class="text-blue-400 w-60 cursor-pointer flex justify-between items-center" @click="open = true">All <i class="fas fa-chevron-down"></i></div>
-                        <ul x-show="open" @click.away="open = false" class="absolute border border-gray-200 bg-white z-30">
-                                @foreach ($department as $team)
-                                <a href="{{route('getTeamByJob', ['team_job' => $team->job])}}">
-                                <li class="text-black hover:bg-blue-400 hover:text-white duration-100 px-5 py-1" value="{{ $team->job }}">{{ $team->job }}</li>
-                                </a>
-                                @endforeach
-                        </ul>
-                </div>
-            </div>
-            <div class="flex items-center">
-                <label class="font-bold text-xl pr-2" for="technology">Industries:</label>
-                <div x-data="{open:false}"
-                    class="p-2 border-none relative outline-none bg-transparent text-blue-500 text-xl font-bold" name="industry" id="industry">
-                    <div class="text-blue-400 w-60 cursor-pointer flex justify-between items-center" @click="open = true">All <i class="fas fa-chevron-down"></i></div>
-                    <ul x-show="open" @click.away="open = false" class="absolute border border-gray-200 bg-white z-30">
-                            <a href="">
-                                <li class="text-black hover:bg-blue-400 hover:text-white duration-100 px-5 py-1" value="">
-
-                                </li>
-                            </a>
-                        </ul>
-                </div>
-            </div>
+        <div class="flex flex-wrap py-5  lg:mx-20 mx-5">
+            @foreach ($department as $team)
+                <a href="{{ route('getTeamByJob', ['team_job' => $team->job]) }}"
+                    class="m-1 py-2 px-8 duration-300 focus:outline-none focus:text-white text-blue-500 bg-blue-100 rounded-3xl focus:border-none focus:bg-blue-500">
+                    <p class="text-sm font-bold">{{ $team->job }}</p>
+                </a>
+            @endforeach
         </div>
         <div class="flex flex-wrap mx-1 overflow-hidden mt-4 justify-center items-start xl:-mx-2">
              @foreach ($teams as $team)
