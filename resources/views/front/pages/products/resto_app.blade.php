4d48b4c82c0ef690e37930708c01e48265985306
Shohruh Qudratullaev <72213836+Shohruh04@users.noreply.github.com>
2021-03-18 15:51:17 +0500

    added img

---
+++ resources/views/front/pages/products/resto_app.blade.php
@@ -0,0 +1,28 @@
+@extends('front.layout')
+
+@section('css')
+
+@endsection
+
+@section('main')
+    <div class="block w-full p-6 lg:p-20">
+    <div class="lg:mt-6" style="background-color: red;">
+        <div class="container mx-auto">
+            <div class="grid grid-cols-1 lg:grid-cols-2 p-6">
+                <div class="flex">
+
+                </div>
+                <div class="flex">
+                    <img src="{{asset('images/products/RestoApp/Screenshot_1.png')}}" alt="">
+                </div>
+            </div>
+        </div>
+    </div>  
+    </div>
+@endsection
+
+
+@section('js')
+
+@endsection
+
